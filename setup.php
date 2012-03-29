206f4d2e257a18504a234ce3b16e835e54dad85b
Kazuo KASHIMA <k4200@kazu.tv>
2012-03-29 20:45:22 +0900

    current_time is ok?

--- setup.php
+++ setup.php
@@ -10,7 +10,7 @@ if ($db = A602_Db::connect()) {
                 url VARCHAR(255),
                 name VARCHAR (100),
                 event_id BIGINT,
-                created DATE DEFAULT (datetime('now','localtime'))
+                created DATE DEFAULT current_time
             )";
   try {
     $db->query($sql);
